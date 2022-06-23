f8a351fd04dd8b0987a7270633fcdfc92fe3de1e
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-06-23 00:03:11 -0500

    Prev and Next buttons

--- src/App.js
+++ src/App.js
@@ -16,6 +16,26 @@ function App() {
     setPage(index);
   };
 
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
   return (
     <main>
       <div className="section-title">
@@ -29,6 +49,9 @@ function App() {
           </div>
           {!loading && (
             <div className="btn-container">
+              <button className="prev-btn" onClick={prevPage}>
+                prev
+              </button>
               {data.map((item, index) => {
                 return (
                   <button
@@ -44,6 +67,9 @@ function App() {
                   </button>
                 );
               })}
+              <button className="next-btn" onClick={nextPage}>
+                next
+              </button>
             </div>
           )}
         </section>
