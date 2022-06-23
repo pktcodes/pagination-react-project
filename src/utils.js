be34e2a8558f5ba07432144ac43e024c991db8b8
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-06-22 19:01:11 -0500

    Paginate Logic

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 9;
+  const pages = Math.ceil(followers.length / itemsPerPage);
 
-export default paginate
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+
+  return newFollowers;
+};
+
+export default paginate;
